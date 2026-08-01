d19ac98757f66f2205911b2c269413ee9650e26f
Pranesh1536 <netrootpranesh@gmail.com>
2026-08-01 16:04:31 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,5 @@
 resource "aws_security_group" "ssh" {
-  name = "sam-backend"
+  name = "pranesh-backend"
 
   # SSH
   ingress {
@@ -48,7 +48,7 @@ resource "aws_instance" "server" {
   vpc_security_group_ids = [aws_security_group.ssh.id]
 
   tags = {
-    Name = "Ansible-Remote-State"
+    Name = "Pranesh-server"
   }
 }
 
